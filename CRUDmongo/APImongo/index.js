f6a716698f0c8df3b1bf9b1e3a1e12a1a21ecd1d
Gilvan Alves <gilvanalves85@outlook.com>
2024-12-02 16:09:04 -0300

    Front-end react crud.

--- CRUDmongo/APImongo/index.js
+++ CRUDmongo/APImongo/index.js
@@ -30,6 +30,42 @@ app.post("/users", async (request, response) => {
     }
 });
 
+app.put("/users/:id", async (req, res) => {
+    try {
+      const { id } = req.params; // ID do usuário a ser atualizado
+      const updatedUser = req.body; // Novos dados do usuário
+  
+      // Atualiza o usuário pelo ID
+      const user = await User.findByIdAndUpdate(id, updatedUser, { new: true });
+  
+      if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" });
+      }
+  
+      return res.status(200).json(user); // Retorna o usuário atualizado
+    } catch (error) {
+      return res.status(400).json({ error: "Erro ao atualizar usuário", details: error.message });
+    }
+  });
+  
+  app.delete("/users/:id", async (req, res) => {
+    try {
+      const { id } = req.params; // ID do usuário a ser deletado
+  
+      // Remove o usuário pelo ID
+      const user = await User.findByIdAndDelete(id);
+  
+      if (!user) {
+        return res.status(404).json({ error: "Usuário não encontrado" });
+      }
+  
+      return res.status(200).json({ message: "Usuário deletado com sucesso" });
+    } catch (error) {
+      return res.status(400).json({ error: "Erro ao deletar usuário", details: error.message });
+    }
+  });
+  
+
 // Conexão com o MongoDB
 mongoose.connect("mongodb+srv://mongo:mongoso@cluster0.pjqam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log('Banco Conectado!'))
